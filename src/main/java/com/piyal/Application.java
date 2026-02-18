bf1f12f3c04755726a7e6c93d8db1264e4b37d3d
David Xiao <71328511+davidhsiaotw@users.noreply.github.com>
2026-02-18 14:26:28 -0800

    chore: print a text

--- src/main/java/com/piyal/Application.java
+++ src/main/java/com/piyal/Application.java
@@ -5,6 +5,7 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("micronaut app is running...");
         Micronaut.run(Application.class, args);
     }
 }
